48262eb7dd547320851b8e6d2bf9debdb33ce62b
Devilz <134226998+Russellbillah@users.noreply.github.com>
2026-05-10 03:17:31 +0600

    Add files via upload

---
+++ russell/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p> <script>document.write(new Date().getFullYear());</script> &copy;Lumineux | Hair Impressions. Developed by NTP Solutions</p>
+    </div>
+        <!--//footer-->
